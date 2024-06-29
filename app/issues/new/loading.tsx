dc95da70f8849c82aeb0371733e802eca88b33fd
VishalChoudharyGithub <vishalchoudhary.mail@gmail.com>
2024-06-29 18:17:32 +0530

    handle loading skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const loadingNewIssuePage = () => {
-  return <div>Loading ...</div>;
+const loadingNewIssuePage = async () => {
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height={"20rem"} />
+    </Box>
+  );
 };
 
 export default loadingNewIssuePage;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -14,7 +14,7 @@ import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
-const NewIssuePage = () => {
+const NewIssuePage = async () => {
   const {
     register,
     control,
@@ -37,7 +37,6 @@ const NewIssuePage = () => {
       setError("An unexpected error occured.");
     }
   });
-
   return (
     <div className="max-w-xl ">
       {error && (
