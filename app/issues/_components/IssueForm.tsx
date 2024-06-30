8ccae52922f05970dd7f0bc8ae6e76a5888e4e96
VishalChoudharyGithub <vishalchoudhary.mail@gmail.com>
2024-06-30 22:05:04 +0530

    Build an api for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import { Spinner, ErrorMessage } from "@/app/components";
 import dynamic from "next/dynamic";
@@ -15,7 +15,7 @@ import { Issue } from "@prisma/client";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = async ({ issue }: { issue?: Issue }) => {
   const {
@@ -24,7 +24,7 @@ const IssueForm = async ({ issue }: { issue?: Issue }) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
   const router = useRouter();
   const [error, setError] = useState<string>("");
