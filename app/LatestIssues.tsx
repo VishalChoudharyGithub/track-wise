4eb3445e9071ab9576aff651d880f6d81d60e44c
VishalChoudharyGithub <vishalchoudhary.mail@gmail.com>
2024-07-26 23:28:07 +0530

    Optimize performance using react cache

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -12,32 +12,33 @@ const LatestIssues = async () => {
       assignedToUser: true,
     },
   });
+
   return (
     <Card>
-      <Heading size={"4"} mb={"5"}>
+      <Heading size="4" mb="5">
         Latest Issues
       </Heading>
       <Table.Root>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Root key={issue.id}>
+            <Table.Row key={issue.id}>
               <Table.Cell>
-                <Flex justify={"between"} align={"center"}>
-                  <Flex direction={"column"} align={"start"} gap={"2"}>
+                <Flex justify="between">
+                  <Flex direction="column" align="start" gap="2">
                     <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                     <IssueStatusBadge status={issue.status} />
                   </Flex>
                   {issue.assignedToUser && (
                     <Avatar
-                      size={"2"}
-                      radius="full"
                       src={issue.assignedToUser.image!}
                       fallback="?"
+                      size="2"
+                      radius="full"
                     />
                   )}
                 </Flex>
               </Table.Cell>
-            </Table.Root>
+            </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
