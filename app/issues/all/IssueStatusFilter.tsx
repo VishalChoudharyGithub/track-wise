4d0bbf222f273d9bd6cc2cbc55f6885beaa851fb
VishalChoudharyGithub <vishalchoudhary.mail@gmail.com>
2024-08-03 00:01:14 +0530

    Remove placeholder from issuestatus filter

--- app/issues/all/IssueStatusFilter.tsx
+++ app/issues/all/IssueStatusFilter.tsx
@@ -37,7 +37,7 @@ const IssueStatusFilter = () => {
         router.push("/issues/all" + query);
       }}
     >
-      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Trigger />
       <Select.Content>
         {statuses.map((status) => (
           <Select.Item key={status.label} value={status.value || ""}>
